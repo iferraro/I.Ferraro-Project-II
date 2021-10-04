96d65ac9cd07b36dd6247687468e3baa81a141a8
iferraro <afronerd01@outlook.com>
2021-10-03 22:11:34 -0700

    Posting to a user profile is enabled

--- controllers/work.js
+++ controllers/work.js
@@ -9,11 +9,11 @@ module.exports = {
 async function show(req, res) {
     try {
         const contribId = req.params.id;
-        console.log(contribId);
+        // console.log(contribId);
         const particularContrib = await Contributor.findById(contribId);
-        console.log(particularContrib);
-        const contribPosts = await Post.find({contributor: contribId}); // finds all posts made by this particular contributors
-        console.log(contribPosts);
+        // console.log(particularContrib);
+        const contribPosts = await Post.find({contributor: contribId}); // finds all posts made by this particular contributor
+        // console.log(contribPosts);
         res.render("profile", { tabTitle: particularContrib.name, heading: particularContrib.name, contribPosts }); // show work of particular contributor
     } catch (err) {
         res.send(err); // if id is not that of a contributor, then error
@@ -23,16 +23,15 @@ async function show(req, res) {
 function newPost(req, res) {
     if (req.user) {
         console.log(req.user);
-        const contribId = req.params.id;
-        res.render("posts/new", { tabTitle: "New Post", heading: "Add Something New to the Site", contribId });
+        res.render("posts/new", { tabTitle: "New Post", heading: "Add Something New to the Site", contribId: req.user._id });
     } else {
-        res.redirect(`/contributors/${contribId}`);
+        res.redirect(`/contributors/${req.params.id}`);
     }
 }
 
 async function create(req, res) {
+    req.body.contributor = req.user._id;
     const newPost = await Post.create(req.body);
-    newPost.contributor = req.user._id;
     console.log(newPost);
     res.redirect(`/contributors/${req.user._id}`);
 }
